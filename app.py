24106ab0ffcf082d37b5307d2e24c9b60ce0902d
Petunia5 <82682501+Petunia5@users.noreply.github.com>
2025-01-13 13:13:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
@@ -9,6 +10,7 @@ with open('model_rf.pkl', 'rb') as model_file:
 with open('scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
+# Initialiser l'application Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -22,7 +24,7 @@ def predict():
     perimeter = data.get('perimeter')
     area = data.get('area')
 
-    # Validation des champs
+    # Vérification des champs
     if perimeter is None or area is None:
         return jsonify({"error": "Champs manquants : 'perimeter' ou 'area'"}), 400
 
@@ -30,9 +32,11 @@ def predict():
     features = np.array([[perimeter, area]])
     features_scaled = scaler.transform(features)
 
-    # Faire la prédiction
+    # Faire une prédiction
     prediction = model.predict(features_scaled)
     return jsonify({"estimated_time": round(prediction[0], 2)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Utiliser le port spécifié par Render ou 5000 par défaut
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
